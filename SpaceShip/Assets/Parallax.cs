3ad9006e57cc76a5aeff050883f5c0530b4d5831
GuiSM6030 <guizinho6030@gmail.com>
2026-09-18 13:38:43 -0300

    Inicio spaceship

---
+++ SpaceShip/Assets/Parallax.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float parallaxEffect = 0.5f;
+
+    private float length;
+
+    void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            length = spriteRenderer.bounds.size.x;
+        }
+    }
+
+    void Update()
+    {
+        if (length <= 0f)
+            return;
+
+        transform.position += Vector3.left * Time.deltaTime * parallaxEffect;
+
+        if (transform.position.x < -length)
+        {
+            transform.position = new Vector3(length, transform.position.y, transform.position.z);
+        }
+    }
+}
